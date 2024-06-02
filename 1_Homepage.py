44dc009db2a1a01e4c5b33f730a93cdc40f57851
Vihanga Mihiranga <50793729+Vihanga22365@users.noreply.github.com>
2024-06-02 10:23:00 +0530

    Create App

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -117,7 +117,7 @@ if "messages" not in st.session_state:
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
-        st.text(message["content"].replace('*', ''))
+        st.markdown(message["content"].replace('*', ''))
 
 
 # React to user input
@@ -135,6 +135,6 @@ if prompt := st.chat_input("How can i help you?"):
     
     
     with st.chat_message("assistant"):
-        st.text(response.replace('*', ''))
+        st.markdown(response.replace('*', ''))
             
     st.session_state.messages.append({"role": "assistant", "content": response})
